af37e30b7be52459693f08c5b29e7d7864a43fcf
Miika Säynevirta <mayse@iki.fi>
2013-11-17 15:40:17 +0200

    drop-tables

---
+++ sql/drop-tables.sql
@@ -0,0 +1,6 @@
+USE saunakanta;
+DROP TABLE IF EXISTS ilmoittautuminen;
+DROP TABLE IF EXISTS saunavuoro;
+DROP TABLE IF EXISTS omistaja;
+DROP TABLE IF EXISTS kayttaja; 
+DROP TABLE IF EXISTS sauna;
