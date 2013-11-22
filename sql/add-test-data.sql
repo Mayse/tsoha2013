5880a654081f67ed6280442919c57c57cad2f303
Miika Säynevirta <miika.saynevirta@iki.fi>
2013-11-22 15:31:17 +0200

    salasanatietue käyttäjään

--- sql/add-test-data.sql
+++ sql/add-test-data.sql
@@ -2,10 +2,10 @@ USE saunakanta;
 INSERT INTO `sauna` (`snimi`, `sijainti`, `koko`) VALUES ('sauna1', 'nurkan takana', 5);
 INSERT INTO `sauna` (`snimi`, `sijainti`, `koko`) VALUES ('sauna2', 'saunakatu 5, portaat alas ja oikealla. Ovikoodi 0202', 25);
 
-INSERT INTO `kayttaja` (`knimi`) VALUES ('saunoja1');
-insert into `kayttaja` (`knimi`) VALUES ('saunoja2');
-insert into `kayttaja` (`knimi`) VALUES ('saunoja3');
-insert into `kayttaja` (`knimi`) VALUES ('saunoja4');
+INSERT INTO `kayttaja` (`knimi`, `salasana`) VALUES ('saunoja1', 'salasana');
+insert into `kayttaja` (`knimi`, `salasana`) VALUES ('saunoja2', 'salasana');
+insert into `kayttaja` (`knimi`, `salasana`) VALUES ('saunoja3', 'salasana';
+insert into `kayttaja` (`knimi`, `salasana`) VALUES ('saunoja4', 'salasana');
 
 INSERT INTO `omistaja` (`knimi`,`snimi`) VALUES ('saunoja1','sauna1');
 INSERT INTO `omistaja` (`knimi`,`snimi`) VALUES ('saunoja2','sauna2');
