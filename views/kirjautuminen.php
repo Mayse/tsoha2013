f6f018178665858a20d33d3e9f4214b92433bc3b
Mayse <mayse@iki.fi>
2013-11-22 14:59:33 +0200

    kirjautumisnäkymä

---
+++ views/kirjautuminen.php
@@ -0,0 +1,30 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <div>
+
+            <form action="login" method="POST">Käyttäjänimi <input type="text" name="kayttajanimi">      
+                <br>
+                Salasana <input type="password" name="salasana">
+                <br>
+                <input type="submit" value="Submit">
+            </form>
+            
+            
+            
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
